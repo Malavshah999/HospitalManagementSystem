a37ea98da31af50af448c9fdc28cb9b4961a192b
Malavshah999 <shahmalav1999@gmail.com>
2020-07-29 23:23:30 +0530

    Initial commit

---
+++ Project/login/admin/index.php
@@ -0,0 +1,95 @@
+<?php
+session_start();
+error_reporting(0);
+include("include/config.php");
+if(isset($_POST['submit']))
+{
+$ret=mysqli_query($con,"SELECT * FROM admin WHERE username='".$_POST['username']."' and password='".$_POST['password']."'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="dashboard.php";//
+$_SESSION['login']=$_POST['username'];
+$_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+$_SESSION['errmsg']="Invalid username or password";
+$extra="index.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Admin-Login</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous"/>
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/styles.css">
+	</head>
+	
+	<body class="login">
+		<div class="row">
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				
+				<div class="logo margin-top-30">
+					<h2 style="text-align: center;color: blue;">Admin Login</h2>
+				</div>
+
+				<div class="box-login">
+					<form class="form-login" method="post">
+						<fieldset>
+							<legend>
+								Sign in to your account
+							</legend>
+							<p>
+								Please enter your name and password to log in.<br />
+								<span style="color:red;"><?php echo htmlentities($_SESSION['errmsg']); ?><?php echo htmlentities($_SESSION['errmsg']="");?></span>
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="text" class="form-control" name="username" placeholder="Username">
+									<i class="fa fa-user"></i>
+								</span>
+							</div>
+							<div class="form-group form-actions">
+								<span class="input-icon">
+									<input type="password" class="form-control password" name="password" placeholder="Password"><i class="fa fa-lock"></i>
+								</span>
+							</div>
+							<div class="form-actions">
+								<button type="submit" class="btn btn-primary pull-right" name="submit">
+									Login 
+									<i class="fa fa-arrow-circle-right"></i>
+								</button>
+					
+								<input type="button" class="btn btn-primary pull-left" value="Go Back!" onclick="history.back(-1)" />
+							</div>
+						
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				Login.init();
+			});
+		</script>	
+	
+	</body>
+	<!-- end: BODY -->
+</html>
