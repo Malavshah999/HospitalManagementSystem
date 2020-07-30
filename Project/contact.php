47fd8949cbdcac3af236d7c4ced77464489f5b56
Malavshah999 <shahmalav1999@gmail.com>
2020-07-30 11:27:15 +0530

    Initial commit

--- Project/contact.php
+++ Project/contact.php
@@ -1,197 +1,182 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Contact</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Health medical template project">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/contact.css">
-<link rel="stylesheet" type="text/css" href="styles/contact_responsive.css">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
-<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
-<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
-<link rel="stylesheet" type="text/css" href="styles/responsive.css">
-<link rel="stylesheet" type="text/css" href="styles/style.css">
+	<title>Contact</title>
 
-<link rel="stylesheet" type="text/css" href="styles/icomoon.css">
-	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="styles/contact.css">
+	<link rel="stylesheet" type="text/css" href="styles/contact_responsive.css">
+	<link rel="stylesheet" type="text/css" href="styles/bootstrap4/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+	<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
 
 </head>
+
 <body>
+	<div class="super_container">
+		<!-- Menu -->
 
-<div class="super_container">
-
-	<!-- Menu -->
-
-	<div class="menu trans_500">
-		<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
-			<div class="menu_close_container"><div class="menu_close"></div></div>
-			<ul>
-				<li class="menu_item"><a href="index.php">Home</a></li>
-				<li class="menu_item"><a href="#">About us</a></li>
-				
-			<li class="menu_item"><a href="contact.php">Contact</a></li>
-			</ul>
+		<div class="menu trans_500">
+			<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+				<div class="menu_close_container"><div class="menu_close"></div></div>
+				<ul>
+					<li class="menu_item"><a href="index.php">Home</a></li>
+					<li class="menu_item"><a href="#">About us</a></li>
+					<li class="menu_item"><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="menu_social">
+				<ul>
+					<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
+					<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+				</ul>
+			</div>
 		</div>
-		<div class="menu_social">
-			<ul>
-				<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
-				<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-			</ul>
-		</div>
-	</div>
 	
-	<!-- Home -->
+		<!-- Home -->
 
-	<div class="home">
-		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/contact.jpg" data-speed="0.8"></div>
+		<div class="home">
+			<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/contact.jpg" data-speed="0.8">
+			</div>
+			
+			<!-- Header -->
 
-	
-		<!-- Header -->
-
-		<header class="header" id="header">
-			<div>
-				<div class="header_top">
-					<div class="container">
+			<header class="header" id="header">
+				<div>
+					<div class="header_top">
+						<div class="container">
 						</div>
 					</div>
 				</div>			
-<section class="navigation">
-  <div class="nav-container">
-    <div class="brand">
-      <a href="#!">LOGO</a>
-    </div>
-    <nav>
-      <div class="nav-mobile">
-        <a id="nav-toggle" href="#!"><span></span></a>
-      </div>
-      <ul class="nav-list">
-        <!-- Setting the links to #! will ensure that no action takes place on click. -->
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-       
-       
-        <li><a href="contact.php">Contact</a></li>
-      
- <li><a href="#!">User Login</a>
-           <ul class="nav-dropdown">
-            <li><a href="login/drlogin.php">Doctor's Login</a></li>
-            <li><a href="login/patientlogin.php">Patient's Login</a></li>
-<li><a href="#">Admin Login</a></li>
-              </ul>
-
-        </li>
- <li><a href="#!">Sign-Up</a>
-          <ul class="nav-dropdown">
-            <li><a href="Signup/doctorsignup/signupdoctor.php">As a Doctor</a></li>
-            <li><a href="Signup/patientsignup/signuppateint.php">As a Patient</a></li>
-              </ul>
-              
-        </li>
- 
-
-      </ul>
-    </nav>
-  </div>
-</section>
-					</div>
-				</div>	
-			</div>
-		</header>
-
-		<div class="home_container">
-			<div class="container">
-				<div class="row">
-					<div class="col">
-						<div class="home_content">
-							<div class="home_title">Contact</div>
+				<section class="navigation">
+					<div class="nav-container">
+						<div class="brand">
+							<a href="">LOGO</a>
 						</div>
+						<nav>
+							<div class="nav-mobile">
+								<a id="nav-toggle" href="#!"><span></span></a>
+							</div>
+							<ul class="nav-list">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="about.php">About</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								<li><a href="#">User Login</a>
+									<ul class="nav-dropdown">
+										<li><a href="login/drlogin.php">Doctor's Login</a></li>
+										<li><a href="login/patientlogin.php">Patient's Login</a></li>
+										<li><a href="login/admin/index.php">Admin Login</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Sign-Up</a>
+									<ul class="nav-dropdown">
+										<li><a href="Signup/doctorsignup/signupdoctor.php">As a Doctor</a></li>
+										<li><a href="Signup/patientsignup/signuppateint.php">As a Patient</a></li>
+									</ul>
+								</li>
+	
+	
+							</ul>
+						</nav>
+					</div>
+				</section>
+			</header>
+		</div>
+	</div>	
+
+	<div class="home_container">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="home_content">
+						<div class="home_title">Contact</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
+	
 	<!-- Contact -->
 
 	<div class="contact">
 		<div class="container">
 			<div class="row">
+				<div class="container2">
+					<div class="innerwrap">
+						<section class="section1 clearfix">
+							<div class="textcenter">
+								<span class="seperator"></span>
+								<h1>Drop Us a Mail</h1>
+							</div>
+						</section>
+						<section class="section2 clearfix">
+							<div class="col2 column1 first">
+								<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script>
+								<div class="sec2map" style='overflow:hidden;height:550px;width:100%;'>
+									<div id='gmap_canvas' style='height:100%;width:100%;'></div>
+									<div>
+										<small><a href="http://embedgooglemaps.com">embed google maps </a></small>
+									</div>
+									<div>
+										<small><a href="http://freedirectorysubmissionsites.com/">free web directories</a></small>
+									</div>
+									<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
+								</div>
+								<script type='text/javascript'>
+									function init_map(){
+										var myOptions = {zoom:14,center:new google.maps.LatLng(19.075314480255834,72.88153973865361),mapTypeId: google.maps.MapTypeId.ROADMAP};
+										map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+										marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(19.075314480255834,72.88153973865361)});
+										infowindow = new google.maps.InfoWindow({content:'<strong>My Location</strong><br>mumbai<br>'});
+										google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);
+									}
+									google.maps.event.addDomListener(window, 'load', init_map);
+								</script>
+							</div>
+							<div class="col2 column2 last">
+								<div class="sec2innercont">
+									<div class="sec2addr">
+										<p>SMT. P.D.HINDUJA TRUST'S, INSTITUTE OF MANAGEMENT STUDIES (31236)</p>
+										<p><span class="collig">Phone :</span> +91 8145220128</p>
+										<p><span class="collig">Email :</span>akramulali8067@gmail.com</p>
+										<p><span class="collig">Fax :</span> +91 7045053487</p>
+									</div>
+								</div>
+								<div class="sec2contactform">
+									<h3 class="sec2frmtitle">Want to Know More?? Drop Us a Mail</h3>
+									<form action="http://www.SnapHost.com/captcha/send.aspx" id="ContactUsCaptchaWebForm" method="post" onsubmit="return ValidateForm(this)" target="_top">
+										<input name="skip_WhereToSend" type="hidden" value="akramulali8067@gmail.com" />
+										<input name="skip_SnapHostID" type="hidden" value="9FCFDTWMBCRB" />
+										<input name="skip_WhereToReturn" type="hidden" value="http://localhost/ProjectLatestNew/project/index.php"/>
+										<input name="skip_Subject" type="hidden" value="Contact Us Form" />
+										<input name="skip_ShowUsersIp" type="hidden" value="1" />
+										<div class="clearfix">
+											<input type="text" class="col2 first" name="FirstName" placeholder="Your First Name">
+											<input type="text" class="col2 last" name="LastName" placeholder="Your Last Name">
+										</div>
+										<div class="clearfix">
+											<input type="email" class="col2 first" name="EmailAddress" placeholder="Your E-mail">
+											<input type="text" class="col2 last" name="Contact" placeholder="Your Contact Number">
+										</div>
+										<div class="clearfix">
+											<textarea name="message" class="col2 first" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+										</div>
+										<div class="clearfix"><input type="submit" value="Send"></div>
 
-			<div class="container2">
-		<div class="innerwrap">
-		
-			<section class="section1 clearfix">
-				<div class="textcenter">
-					<span class="seperator"></span>
-					<h1>Drop Us a Mail</h1>
-				</div>
-			</section>
-		
-			<section class="section2 clearfix">
-				<div class="col2 column1 first">
-					<script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div class="sec2map" style='overflow:hidden;height:550px;width:100%;'><div id='gmap_canvas' style='height:100%;width:100%;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="http://freedirectorysubmissionsites.com/">free web directories</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:14,center:new google.maps.LatLng(19.075314480255834,72.88153973865361),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(19.075314480255834,72.88153973865361)});infowindow = new google.maps.InfoWindow({content:'<strong>My Location</strong><br>mumbai<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
-				</div>
-				<div class="col2 column2 last">
-					<div class="sec2innercont">
-						<div class="sec2addr">
-							<p>SMT. P.D.HINDUJA TRUST'S, INSTITUTE OF MANAGEMENT STUDIES (31236)</p>
-							<p><span class="collig">Phone :</span> +91 8145220128</p>
-							<p><span class="collig">Email :</span>akramulali8067@gmail.com</p>
-							<p><span class="collig">Fax :</span> +91 7045053487</p>
-						</div>
-					</div>
-					<div class="sec2contactform">
-						<h3 class="sec2frmtitle">Want to Know More?? Drop Us a Mail</h3>
-					<form action="http://www.SnapHost.com/captcha/send.aspx" id="ContactUsCaptchaWebForm" method="post" onsubmit="return ValidateForm(this)" target="_top">
-<input name="skip_WhereToSend" type="hidden" value="akramulali8067@gmail.com" />
-<input name="skip_SnapHostID" type="hidden" value="9FCFDTWMBCRB" />
-<input name="skip_WhereToReturn" type="hidden" value="http://localhost/ProjectLatestNew/project/index.php"/>
-<input name="skip_Subject" type="hidden" value="Contact Us Form" />
-<input name="skip_ShowUsersIp" type="hidden" value="1" />
-		<div class="clearfix">
-								 <input type="text" class="col2 first" name="FirstName" placeholder="Your First Name">
-                        <input type="text" class="col2 last" name="LastName" placeholder="Your Last Name">
-                       	</div>
-							<div class="clearfix">
-								<input type="email" class="col2 first" name="EmailAddress" placeholder="Your E-mail">
-                    <input type="text" class="col2 last" name="Contact" placeholder="Your Contact Number">
-                      		</div>
-							<div class="clearfix"><textarea name="message" class="col2 first" id="message" cols="30" rows="10" placeholder="Message"></textarea>
-                         
+									</form>
+								</div>
 
 							</div>
-
-<div class=" input-group">   
-                           <p><i style="font-family:'Saira Condensed', sans-serif;color:black;">   Enter web form code*:&nbsp;&nbsp;</i></p><input name="skip_CaptchaCode" type="text" style="width:80px;padding:5px;color:black;" maxlength="6" />
-                            <a href="http://www.snaphost.com/captcha/ReadyForms/ContactUsForm.aspx"><img id="CaptchaImage" alt="Check your connection" title="HTML code for Contact Us form"
-style="margin-left:20px;"
-src="http://www.SnapHost.com/captcha/CaptchaImage.aspx?id=9FCFDTWMBCRB&ImgType=2" /></a>
-<p>
-<center><a href="#" onclick="return ReloadCaptchaImage('CaptchaImage');">&nbsp;<span style="font-size:16px;text-decoration:none;color:black;font-family:'Saira Condensed', sans-serif;" class="input-group">RELOAD IMAGE</span></a> </center></p>
-                          </div></center>
-
-							<div class="clearfix"><input type="submit" value="Send"></div>
-
-						</form>
+						</section>
 					</div>
-
 				</div>
-			</section>
-		
+			</div>
 		</div>
-	</div>
-			</div></div>
 	</div>
 
 	<!-- Footer -->
@@ -201,7 +186,6 @@ src="http://www.SnapHost.com/captcha/CaptchaImage.aspx?id=9FCFDTWMBCRB&ImgType=2
 		<div class="footer_content">
 			<div class="container">
 				<div class="row">
-
 					<!-- Footer About -->
 					<div class="col-lg-3 footer_col">
 						<div class="footer_about">
@@ -211,21 +195,21 @@ src="http://www.SnapHost.com/captcha/CaptchaImage.aspx?id=9FCFDTWMBCRB&ImgType=2
 							<div class="footer_about_text">coming soon</div>
 							<div class="footer_social">
 								<ul class="d-flex flex-row align-items-center justify-content-start">
-									<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-									<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-									<li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
-									<li><a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a></li>
-									<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+									<li><a href="https://www.instagram.com/accounts/login/?hl=en" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="https://twitter.com/login?lang=en-gb" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 								</ul>
 							</div>
-							<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This project is made with <i class="fa fa-heart-o" aria-hidden="true"></i>by Akramul and Malav</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-</div>
+							<div class="copyright">
+								Copyright &copy;
+								<script>document.write(new Date().getFullYear());
+								</script> 
+								All rights reserved | This project is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by Akramul and Malav</a>
+							</div>
 						</div>
 					</div>
-<!-- Footer Contact -->
+					
+					<!-- Footer Contact -->
 					<div class="col-lg-5 footer_col">
 						<div class="footer_contact">
 							<div class="footer_contact_title">Quick Contact</div>
@@ -269,14 +253,9 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 				</div>
 			</div>
 		</div>
-		</div>
 	</footer>
-</div>
-
 <script src="js/jquery-3.3.1.min.js"></script>
-<script src="styles/bootstrap4/popper.js"></script>
 <script src="styles/bootstrap4/bootstrap.min.js"></script>
-<script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
 <script src="plugins/easing/easing.js"></script>
 <script src="plugins/parallax-js-master/parallax.min.js"></script>
 <script src="js/custom.js"></script>
