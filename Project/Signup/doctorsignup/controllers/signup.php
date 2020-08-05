6fd4b0dcc32062f89f2bcdb30c05c24b70f3e078
yourname <you@example.com>
2020-08-05 19:36:20 +0530

    doctor login complete with doctor signup and i also give u database please import this database also..

--- Project/Signup/doctorsignup/controllers/signup.php
+++ Project/Signup/doctorsignup/controllers/signup.php
@@ -11,7 +11,8 @@ $signup_errors = [];
 
 if (!empty($_POST['email'])) {
     $email = mysqli_real_escape_string($db, $_POST['email']);
-    $query = "SELECT * FROM doctorsignup WHERE email = '$email' ";
+
+    $query = "SELECT * FROM patientsignup WHERE email = '$email' ";
     $result = mysqli_query($db, $query);
     $user = mysqli_fetch_assoc($result);
 
@@ -26,6 +27,7 @@ if (!empty($_POST['email'])) {
     }
 }
 
+
 // Validate the password
 if(!empty($_POST['password']) && !empty($_POST['pw_confirm'])){
     $password = $_POST['password'];
@@ -34,14 +36,16 @@ if(!empty($_POST['password']) && !empty($_POST['pw_confirm'])){
     if ($password !== $pw_confirm) {
         array_push($signup_errors, 'Both passwords must be same!');
     }
+
     if (strlen($password) < 8) {
         array_push($signup_errors, 'Password length must be minimum 8 characters!');
     }
 	else{
-        // store the email in the session
+        // store the password in the session
         $_SESSION['password'] = $password;
     }
 }
+
 
 // If there is no error in the validation, store in the database
 if(count($signup_errors) > 0){
@@ -51,19 +55,21 @@ if(count($signup_errors) > 0){
     // insert the data in the database
     $password = md5($password);
     $query = "INSERT INTO doctorsignup (fullname,phonenumber,email, password)
-			VALUES('$fullname','$phonenumber','$email', '$password')";
+	VALUES('$fullname','$phonenumber','$email', '$password')";
     mysqli_query($db, $query);
+
     // Last inserted row id
     //$last_id = mysqli_insert_id($db);
 
     // Retrieve the last row
-    //$row = mysqli_query($db, "SELECT * FROM doctorsignup WHERE id= $last_id");
+    //$row = mysqli_query($db, "SELECT * FROM patientsignup WHERE id= $last_id");
     //$user_details = mysqli_fetch_array($row, MYSQLI_ASSOC);
     //$_SESSION['user_details'] = $user_details;
 
     // Redirect to Home Dashboard
-   echo"<script> location.href='../../../login/drlogin.php'</script>";
+     echo"<script> location.href='../../../login/doctor/index.php'</script>";
 }
+?>
 
 
 
