1b167536d2adb597837305169a77229077a80b85
Malavshah999 <shahmalav1999@gmail.com>
2020-09-18 15:49:46 +0530

    Final Commit

--- Project/login/admin/load_data.php
+++ Project/login/admin/load_data.php
@@ -23,7 +23,7 @@ check_login();
       while($row = mysqli_fetch_array($result))  
       {  
           $output .= '<div class="big-box"><div class="outer-box">'.$row["room_name"].'<br>Bed Available : '.$row["bed_available"];  
-          $output .= '<br><br><button type="button" data-toggle="modal" data-target="#myModal1" class="btn btn-transparent btn-xs" tooltip="Edit"><i class="fa fa-pencil"></i></button>';
+          $output .= '<br><br><button type="button" data-toggle="modal" data-target="#myModal2" class="btn btn-transparent btn-xs" tooltip="Edit"><i class="fa fa-pencil"></i></button>';
           $output .= '</div></div>';
      }  
       echo $output;  
